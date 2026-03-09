842011e2ab8289c0e29f02642c974e9e752e9569
denismalyi2204 <denismalyi2204@gmail.com>
2026-03-09 15:09:22 +0000

    patched hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 #include <string>
+
+int main() {
+    std::string name;
+    std::cout << "Please enter name: ";
+    std::cin >> name;
+    std::cout << "Hello world from " << name;
+}#include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
